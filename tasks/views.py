913efcdc3899ed73a14def7f034430ef1f27ddf7
Phil <phil.jim.cooper@gmail.com>
2025-03-04 18:12:03 +0000

    tasks displaying and action buttons added

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 
 # Create your views here.
 
 def index(request):
-    tasks = Task.objects.all()
-    return render(request, 'tasks/index.html', {'tasks': tasks})
+    tasks_due = Task.objects.filter(complete=False)
+    tasks_completed = Task.objects.filter(complete=True)
+    return render(request, 'tasks/index.html', {'tasks_due': tasks_due, 'tasks_completed': tasks_completed})
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.complete = True
+    task.save()
+    return redirect('index')
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('index')
+
+context = {"complete_task": complete_task, "delete_task": delete_task}
