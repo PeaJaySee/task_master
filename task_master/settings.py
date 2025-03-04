34ed43bea8b7cec83c7df0e72c0f9c401426a66c
Phil <phil.jim.cooper@gmail.com>
2025-03-04 19:42:38 +0000

    security measures

--- task_master/settings.py
+++ task_master/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*umjtjf(t=2-z78%i)zp-=n1chh@*sfhuz_76bdtw49t1r8+rn'
+SECRET_KEY =  os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
